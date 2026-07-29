9b6b4e728ed1d7829e919d397889c49294082736
utkarshjain1338-hub <utkarshjain1338@gmail.com>
2026-07-29 21:08:33 +0530

    deployed on stellar

--- contracts/escrow/src/lib.rs
+++ contracts/escrow/src/lib.rs
@@ -6,7 +6,7 @@ extern crate std;
 
 mod types;
 
-use soroban_sdk::{contract, contractimpl, symbol_short, token, Address, Env, Symbol};
+use soroban_sdk::{contract, contractimpl, symbol_short, token, Address, Env};
 
 pub use types::{
     EscrowCreatedEvent, EscrowData, EscrowError, EscrowStatus, EscrowView, FundsDepositedEvent,
